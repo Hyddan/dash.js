6edcee27a10e174d873af28357b0155f96260b5e
Daniel Hedenius <daniel.hedenius@gmail.com>
2015-01-15 15:00:44 +0100

    Readded cdmData & Implemented 2 more EME specs
    Also refactored existing EME implementations a bit.

--- src/streaming/vo/protection/ProtectionData.js
+++ src/streaming/vo/protection/ProtectionData.js
@@ -33,19 +33,19 @@
  * Data used to customize KeySystems and override default or CDM-provided
  * values
  *
- * @param {string} laURL a license acquisition URL to use with this key system
- * @param {Object} httpRequestHeaders headers to add to the http request
- * @param {Object} bearerToken
- * @param {Object} clearkeys defines a set of clear keys that are available to
+ * @param {String} laUrl
+ * @param {Object} headers
+ * @param {String} cdmData
+ * @param {Object} clearKeys defines a set of clear keys that are available to
  * the key system.  Object properties are base64-encoded keyIDs (with no padding).
  * Corresponding property values are keys, base64-encoded (no padding).
  * @constructor
  */
-MediaPlayer.vo.protection.ProtectionData = function(laURL, httpRequestHeaders, bearerToken, clearkeys) {
-    this.laURL = laURL;
-    this.httpRequestHeaders = httpRequestHeaders;
-    this.bearerToken = bearerToken;
-    this.clearkeys = clearkeys;
+MediaPlayer.vo.protection.ProtectionData = function(laUrl, headers, cdmData, clearKeys) {
+    this.laUrl = laUrl;
+    this.headers = headers;
+    this.cdmData = cdmData;
+    this.clearKeys = clearKeys;
 };
 
 MediaPlayer.vo.protection.ProtectionData.prototype = {
