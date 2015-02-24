7aa1ffc93b8aca2578e7d7d77c48c944703ca16a
Daniel Hedenius <daniel.hedenius@gmail.com>
2015-02-24 16:52:24 +0100

    Seek after language switch

--- src/streaming/Stream.js
+++ src/streaming/Stream.js
@@ -242,6 +242,7 @@ MediaPlayer.dependencies.Stream = function () {
         switchLanguage = function (lang) {
             var self = this,
                     type = 'audio',
+					currentTime = self.videoModel.getCurrentTime() - 0.1,
                     processor = streamProcessors.filter(function (item) { return type === item.getType(); })[0],
                     bufferController = processor.getBufferController(),
                     buffer = bufferController.getBuffer(),
@@ -262,6 +263,7 @@ MediaPlayer.dependencies.Stream = function () {
             self.adapter.updateData(processor);
             processor.stop();
             processor.start();
+			self.videoModel.setCurrentTime(currentTime);
         },
 
         initializeMediaForType = function(type, manifest, lang) {
